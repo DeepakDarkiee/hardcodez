4c1b95d407574163e678810c45976b984955ea97
Deepak <mdipakpatidar@gmail.com>
2022-11-07 23:55:36 +0530

    fix: env

--- hardcodez/settings/base.py
+++ hardcodez/settings/base.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 
 from django.contrib.messages import constants as messages
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
