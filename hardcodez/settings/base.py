9f00bf1c3092765d3c301ab5580fd83627b5be08
Deepak <mdipakpatidar@gmail.com>
2022-11-08 00:56:46 +0530

    fix: staticfiles

--- hardcodez/settings/base.py
+++ hardcodez/settings/base.py
@@ -124,11 +124,11 @@ STATIC_URL = "/static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+STATIC_ROOT = 'staticfiles'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATIC_URL = "/static/"
-
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
